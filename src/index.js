9eb0569142bbd59f7c3e0662257e9a287ed81fa8
gentaliana <gentaliana@gmail.com>
2020-09-28 19:35:18 +0400

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+exports.min = function min(array = []) {
+    if (array.length === 0) {
+        return 0;
+    } else {
+        return Math.min(...array);
+    }
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array = []) {
+    if (array.length === 0) {
+        return 0;
+    } else {
+        return Math.max(...array);
+    }
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array = []) {
+    if (array.length === 0) return 0;
+    let sum = array.reduce((a, b) => a + b, 0);
+    return sum / array.length;
+};
